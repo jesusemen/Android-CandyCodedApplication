cb19a4814e7d67b6f334ea3c2a8cb6ad34995052
ligherodigi <ligheroenergy@gmail.com>
2020-10-03 12:25:42 +0100

    Task Completed

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -85,6 +85,16 @@ public class MainActivity extends AppCompatActivity {
     }
     // ***
     // TODO - Task 1 - Show Store Information Activity
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent infoIntent = new Intent(this, InfoActivity.class);
+        startActivity(infoIntent);
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
     // ***
 
     private void addCandiesToDatabase(Candy[] candies) {
